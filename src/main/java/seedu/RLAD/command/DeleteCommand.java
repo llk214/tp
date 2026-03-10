27d52a53813601d64f75d9ee5797d7bfc20a18b2
Roshan <rohanroshan2403@gmail.com>
2026-03-10 23:18:13 +0800

    Complete DeleteCommand implementation

--- src/main/java/seedu/RLAD/command/DeleteCommand.java
+++ src/main/java/seedu/RLAD/command/DeleteCommand.java
@@ -1,23 +1,49 @@
 package seedu.RLAD.command;
 
+import seedu.RLAD.Transaction;
 import seedu.RLAD.TransactionManager;
 import seedu.RLAD.Ui;
+import seedu.RLAD.exception.RLADException;
 
-public class DeleteCommand extends Command{
+public class DeleteCommand extends Command {
     public DeleteCommand(String rawArgs) {
         super(rawArgs);
     }
 
+    private String parseHashId() {
+        if (rawArgs == null) {
+            return null;
+        }
+        int idx = rawArgs.indexOf("--hashID");
+        if (idx == -1) {
+            return null;
+        }
+        String afterFlag = rawArgs.substring(idx + "--hashID".length()).trim();
+        // Take only the first token (the ID value)
+        String[] tokens = afterFlag.split("\\s+", 2);
+        return tokens.length > 0 && !tokens[0].isEmpty() ? tokens[0] : null;
+    }
+
     @Override
-    public void execute(TransactionManager transactions, Ui ui) {
-        // TODO: implement parsing of rawArgs
-        // and interaction with TransactionManager
-        ui.showResult("DeleteCommand logic will be implemented here.");
+    public void execute(TransactionManager transactions, Ui ui) throws RLADException {
+        if (!hasValidArgs()) {
+            throw new RLADException("Missing required field: --hashID");
+        }
+
+        String hashId = parseHashId();
+        Transaction toDelete = transactions.findTransaction(hashId);
+        if (toDelete == null) {
+            throw new RLADException("Transaction not found: " + hashId);
+        }
+
+        transactions.deleteTransaction(hashId);
+        ui.showResult(String.format(
+                "Transaction deleted successfully!\n   HashID: %s\n   %s",
+                hashId, toDelete.toString()));
     }
 
     @Override
     public boolean hasValidArgs() {
-        // Implement args check format
-        return true;
+        return rawArgs != null && rawArgs.contains("--hashID") && parseHashId() != null;
     }
 }
