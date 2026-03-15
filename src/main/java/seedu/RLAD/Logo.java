8501aed701e091e33cbae236b819360d1231e921
Koh Li Tian <e1398429@u.nus.edu>
2026-03-16 00:26:50 +0800

    Changed logo design to make it UTF-8 compatible

--- src/main/java/seedu/RLAD/Logo.java
+++ src/main/java/seedu/RLAD/Logo.java
@@ -2,16 +2,16 @@ package seedu.RLAD;
 
 public class Logo {
     private static final String PADDING = "               ";
-    private static final String TOP_LINE = PADDING +    "╔════════════════════════════════════════════════╗";
-    private static final String BOTTOM_LINE = PADDING + "╚════════════════════════════════════════════════╝";
+    private static final String TOP_LINE = PADDING +    "+================================================+";
+    private static final String BOTTOM_LINE = PADDING + "+================================================+";
     private static final String LOGO =
-            PADDING + "║       ██████╗  ██╗       █████╗  ██████╗       ║\n" +
-                    PADDING + "║       ██╔══██╗ ██║      ██╔══██╗ ██╔══██╗      ║\n" +
-                    PADDING + "║       ██████╔╝ ██║      ███████║ ██║  ██║      ║\n" +
-                    PADDING + "║       ██╔══██╗ ██║      ██╔══██║ ██║  ██║      ║\n" +
-                    PADDING + "║       ██║  ██║ ███████╗ ██║  ██║ ██████╔╝      ║\n" +
-                    PADDING + "║       ╚═╝  ╚═╝ ╚══════╝ ╚═╝  ╚═╝ ╚═════╝       ║\n" +
-                    PADDING + "║              Record Losses And Debt            ║";
+            PADDING + "|       ██████╗  ██╗       █████╗  ██████╗       |\n" +
+                    PADDING + "|       ██╔══██╗ ██║      ██╔══██╗ ██╔══██╗      |\n" +
+                    PADDING + "|       ██████╔╝ ██║      ███████║ ██║  ██║      |\n" +
+                    PADDING + "|       ██╔══██╗ ██║      ██╔══██║ ██║  ██║      |\n" +
+                    PADDING + "|       ██║  ██║ ███████╗ ██║  ██║ ██████╔╝      |\n" +
+                    PADDING + "|       ╚═╝  ╚═╝ ╚══════╝ ╚═╝  ╚═╝ ╚═════╝       |\n" +
+                    PADDING + "|              Record Losses And Debt            |";
 
     public static void printRLAD() {
         System.out.println(TOP_LINE);
