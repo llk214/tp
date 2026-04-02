8194dcce410f4e15cc4d984db97edddedd09c24d
Roshan Alagar <roshanalagarpremkumar@gmail.com>
2026-04-02 19:51:31 +0800

    Fix checkstyle indentation in BudgetManager

--- src/main/java/seedu/RLAD/budget/BudgetManager.java
+++ src/main/java/seedu/RLAD/budget/BudgetManager.java
@@ -275,9 +275,7 @@ public class BudgetManager {
     }
     public String getYearlySummary(int year) {
         String[] monthNames =
-                {"January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"
-                };
+                {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
         StringBuilder sb = new StringBuilder();
         sb.append("=== YEARLY BUDGET SUMMARY FOR ").append(year).append(" ===\n\n");
